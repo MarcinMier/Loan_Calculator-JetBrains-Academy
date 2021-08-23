77277794f321e15107d449686ab8482cacf075c9
MarcinMier <75903311+MarcinMier@users.noreply.github.com>
2021-08-23 14:12:28 +0200

    Update main.py
    Update to the proper data

--- main.py
+++ main.py
@@ -1,269 +1,86 @@
-import random
-import sqlite3
-import itertools
-
-db_container = sqlite3.connect('card.s3db')
+from math import ceil, log, floor
+import argparse
 
 
-def check_and_create_db():
-    db_cursor = db_container.cursor()
-    db_cursor.execute("CREATE TABLE IF NOT EXISTS card("
-                      "id INTEGER, "
-                      "number TEXT, "
-                      "pin TEXT, "
-                      "balance INTEGER DEFAULT 0);")
+def calculation(no_in, per):
+    return no_in * pow((1 + no_in), per) / (pow(1 + no_in, per) - 1)
 
 
-def take_user_command():
-    print("1. Create an account\n"
-          "2. Log into account\n"
-          "0. Exit\n")
-
-    command = input()
-    return command
+def logarithm(mon, nom, pri):
+    return log((mon / (mon - (nom * pri))), (1 + nom))
 
 
-def create_user_account():
-    card_number = generate_new_card_number()
-    pin_code = generate_new_pin_code()
+parser = argparse.ArgumentParser(description="This program prints recipes consisting of the ingredients you provide.")
 
-    db_cursor = db_container.cursor()
-    db_cursor.execute("INSERT INTO card(id, number, pin) VALUES " + "("
-                      + str(random.randint(0, 999999)) + ", "
-                      + str(card_number) + ", "
-                      + str(pin_code)
-                      + ");")
+parser.add_argument("--type", choices=["annuity", "diff"])
+parser.add_argument("--principal")
+parser.add_argument("--payment")
+parser.add_argument("--periods")
+parser.add_argument("--interest")
 
-    db_cursor.execute("COMMIT;")
+args = parser.parse_args()
 
-    print("Your card has been created\n"
-          "Your card number:\n" +
-          str(card_number) +
-          "\nYour card PIN:\n" +
-          str(pin_code) + "\n")
+if args.type == 'annuity':
 
+    if args.principal and args.payment and args.interest and args.periods is None:
 
-def generate_new_card_number():
-    db_cursor = db_container.cursor()
-    db_cursor.execute("SELECT number FROM card")
-    results = db_cursor.fetchall()
-    results = [i for i in itertools.chain(*results)]
-
-    bank_identification_number = '400000'
-
-    while True:
-        card_template = bank_identification_number + \
-                        str(format(random.randint(000000000, 999999999), '09d'))
-
-        number_to_check = [int(digit) for digit in card_template]
-
-        for digit in range(0, len(number_to_check), 2):
-            number_to_check[digit] *= 2
-
-        for digit in range(len(number_to_check)):
-            if number_to_check[digit] > 9:
-                number_to_check[digit] -= 9
-        checksum = sum(number_to_check)
-        if checksum % 10 == 0:
-            appendix = 0
+        args.principal = float(args.principal)
+        args.payment = float(args.payment)
+        nominal_i = float(args.interest) / 1200
+        number_of_months = logarithm(args.payment, nominal_i, args.principal)
+        c_number_of_months = ceil(number_of_months)  # number of months of paying by client
+        years = floor(c_number_of_months / 12)
+        reminder = c_number_of_months % 12
+        if reminder == 0 and years != 0:
+            print(f'It will take {years} years to repay this loan!')
+        elif years > 0 and reminder != 0:
+            print(f'It will take {years} years and {reminder} months to repay this loan!')
         else:
-            appendix = 10 - checksum % 10
-        if card_template + str(appendix) in results:
-            continue
-        else:
-            card_number = card_template + str(appendix)
-            return card_number
+            print(f'It will take {reminder} months to repay this loan!')
+        print(f'Overpayment = {int(args.payment * c_number_of_months - args.principal)}')  # printing overpayment
 
+    elif args.principal and args.periods and args.interest and args.payment is None:
 
-def generate_new_pin_code():
-    pin_code = str(format(random.randint(0000, 9999), '04d'))
-    return pin_code
+        args.principal = float(args.principal)
+        args.periods = float(args.periods)
+        n_i = float(args.interest) / 1200  # nominal interest
 
+        monthly_payment = args.principal * calculation(n_i, args.periods)
+        monthly_payment_fc = ceil(monthly_payment)
+        print(f'Your annuity payment = {monthly_payment_fc}!')
+        print(f'Overpayment = {int(monthly_payment_fc * args.periods - args.principal)}')
 
-def is_luhn_checksum(number_to_check):
-    last_number = number_to_check[-1]
-    number_to_check = number_to_check[0:-1]
+    elif args.payment and args.periods and args.interest and args.principal is None:
 
-    number_to_check = [int(digit) for digit in number_to_check]
+        args.payment = float(args.payment)
+        args.periods = float(args.periods)
+        nominal_i = float(args.interest) / 1200
+        principal = floor(args.payment / calculation(nominal_i, args.periods))
+        o_v = args.payment * args.periods - principal  # overpayment
+        print(f"Your loan principal = {principal}!")
+        print(f'Overpayment = {int(o_v)}')
 
-    for digit in range(0, len(number_to_check), 2):
-        number_to_check[digit] *= 2
-
-    for digit in range(len(number_to_check)):
-        if number_to_check[digit] > 9:
-            number_to_check[digit] -= 9
-
-    number_to_check.append(int(last_number))
-    return sum(number_to_check) % 10 == 0
-
-
-def is_account_exist(user_number):
-    db_cursor = db_container.cursor()
-    db_cursor.execute("SELECT number FROM card")
-    results = db_cursor.fetchall()
-    results = [i for i in itertools.chain(*results)]
-    return user_number in results
-
-
-def is_pin_correct(user_number, user_pin):
-    db_cursor = db_container.cursor()
-    db_cursor.execute("SELECT pin FROM card WHERE number = '"
-                      + user_number + "';")
-    actual_pin = db_cursor.fetchone()
-    return user_pin == actual_pin[0]
-
-
-def processing_logged_user(user_number):
-    print("You have successfully logged in!\n")
-    current_card = user_number
-    while True:
-        print("1. Balance\n"
-              "2. Add income\n"
-              "3. Do transfer\n"
-              "4. Close account\n"
-              "5. Log out\n"
-              "0. Exit\n")
-        account_command = input()
-        if account_command.isdigit() and account_command == "1":
-            balance = fetch_user_balance(current_card)
-            print("Balance: " + str(balance) + "\n")
-
-        elif account_command.isdigit() and account_command == "2":
-            deposit_amount = request_income()
-            if not deposit_amount:
-                continue
-            else:
-                add_income(current_card, deposit_amount)
-
-        elif account_command.isdigit() and account_command == "3":
-            process_money_transfer(current_card)
-
-        elif account_command.isdigit() and account_command == "4":
-            close_account(current_card)
-            break
-
-        elif account_command.isdigit() and account_command == "5":
-            print("You have successfully logged out!\n")
-            break
-
-        elif account_command.isdigit() and account_command == "0":
-            print("Bye!")
-            exit(0)
-
-
-def fetch_user_balance(card_number):
-    db_cursor = db_container.cursor()
-    db_cursor.execute("SELECT balance FROM card WHERE number = '" + card_number + "';")
-    balance = db_cursor.fetchone()[0]
-    return balance
-
-
-def request_income():
-    while True:
-        deposit_amount = input("Enter income:\n")
-        if deposit_amount == "0":
-            return False
-        elif deposit_amount.isdigit() and int(deposit_amount) > 0:
-            return deposit_amount
-
-        else:
-            print("Wrong input, try again.\n"
-                  "Input 0 for abort.")
-
-
-def add_income(card_number, deposit_amount):
-    db_cursor = db_container.cursor()
-    db_cursor.execute("SELECT balance FROM card WHERE number = '"
-                      + card_number + "';")
-    balance = db_cursor.fetchone()[0]
-
-    balance = str(balance + int(deposit_amount))
-    db_cursor.execute("UPDATE card "
-                      "SET balance = " + balance +
-                      " WHERE number = '" + card_number + "';")
-    db_cursor.execute("COMMIT;")
-    print("Income was added!\n")
-
-
-def process_money_transfer(source_card):
-    destination_card = input("Transfer\n"
-                             "Enter card number:\n")
-
-    if is_luhn_checksum(destination_card):
-        if is_account_exist(destination_card) or destination_card == source_card:
-
-            transfer_amount = request_transfer_amount()
-            if transfer_amount <= fetch_user_balance(source_card):
-                make_transaction(source_card, destination_card, transfer_amount)
-            else:
-                print("Not enough money!\n")
-                return
-        else:
-            print("Such a card does not exist.")
     else:
-        print("Probably you made a mistake in the card number. "
-              "Please try again!\n")
-        return
+        print('Incorrect parameters.')
+############################################################
+############################################################
 
+if args.type == 'diff':
 
-def request_transfer_amount():
-    transfer_amount = int(input("Enter how much money you want to transfer:"
-                                "\n"))
-    return transfer_amount
+    if args.periods and args.interest and args.principal and args.payment is None:
+        args.principal = float(args.principal)
+        args.periods = float(args.periods)
+        n_i = float(args.interest) / 1200  # nominal interest
+        m = 1  # current repayment month
+        c = 0  # counter of overpayment
+        d = 0  # difference after using ceil()
+        while m <= args.periods:
+            diff_payment = (args.principal / args.periods) + (n_i * (args.principal - (args.principal * (m - 1) / args.periods)))
+            d = ceil(diff_payment)
+            print(f'Month {m}: payment is {d}')
+            m += 1
+            c = c + d
+        print(f'Overpayment = {int(c - args.principal)}')
 
-
-def make_transaction(source_card, destination_card, transfer_amount):
-    db_cursor = db_container.cursor()
-    db_cursor.execute("SELECT balance FROM card WHERE number = '"
-                      + source_card + "';")
-    source_balance = db_cursor.fetchone()[0]
-    db_cursor.execute("UPDATE card "
-                      "SET balance = " + str(source_balance - transfer_amount)
-                      + " WHERE number = '" + source_card + "';")
-
-    db_cursor.execute("SELECT balance FROM card WHERE number = '"
-                      + destination_card + "';")
-    dest_balance = db_cursor.fetchone()[0]
-    db_cursor.execute("UPDATE card "
-                      "SET balance = " + str(dest_balance + transfer_amount)
-                      + " WHERE number = '" + destination_card + "';")
-    db_cursor.execute("COMMIT;")
-    print("Success!\n")
-
-
-def close_account(card_number):
-    db_cursor = db_container.cursor()
-    db_cursor.execute("DELETE FROM card WHERE number = '"
-                      + card_number + "';")
-    db_cursor.execute("COMMIT;")
-    print("The account has been closed!\n")
-
-
-def main():
-    while True:
-
-        command = take_user_command()
-
-        if command.isdigit() and command == "1":
-            create_user_account()
-
-        elif command.isdigit() and command == "2":
-
-            user_number = input("Enter your card number:\n")
-            user_pin = input("Enter your PIN:\n")
-
-            if is_account_exist(user_number) and (
-                    is_pin_correct(user_number, user_pin)
-            ):
-
-                processing_logged_user(user_number)
-
-            else:
-                print("Wrong card number or PIN!\n")
-
-        elif command.isdigit() and command == "0":
-            exit(0)
-
-
-check_and_create_db()
-main()
+    else:
+        print('Incorrect parameters.')
